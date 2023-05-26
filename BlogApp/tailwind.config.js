da2671057f25f6b5e0a7e07bb781d7ee11dda5ae
Mirai3103 <huuhoag1412@gmail.com>
2023-05-26 23:54:28 +0700

    reponsive

--- BlogApp/tailwind.config.js
+++ BlogApp/tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     mode: "jit",
-    content: ["./Views/**/*.{js,ts,jsx,tsx,html,cshtml}"],
+    content: ["./Views/**/*.{js,ts,jsx,tsx,html,cshtml}", "./node_modules/flowbite/**/*.js"],
     theme: {
         extend: {
             screens: {
-                $ssm: { max: "550px" },
-                $sm: { max: "639px" },
-                $md: { max: "767px" },
-                $lg: { max: "1023px" },
-                $xl: { max: "1279px" },
+         
+              
+            
+                $xl: { max: "1279px" }, $lg: { max: "1023px" }, $md: { max: "767px" }, $sm: { max: "639px" }, $ssm: { max: "550px" },
+
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ]
 };
